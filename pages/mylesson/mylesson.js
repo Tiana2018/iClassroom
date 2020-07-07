a3b2801477eefc73826dd1538f0a78fde3295454
Tiana2018 <email@917803833@qq.com>
2020-07-07 15:19:56 +0800

    history

--- pages/mylesson/mylesson.js
+++ pages/mylesson/mylesson.js
@@ -5,7 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
+    show: false,
+    active: 0,
+    logs:["a","b","c"],
+    product: [
+      {
+        _id: '01',
+        dec: '提高英语成绩，备战四六级考试',
+        detail: '提高英语成绩备战四级考试',
+        location: '6月28日开课！',
+        name: '人工智能与信息社会',
+        price: '199',
+        rate: '最受欢迎课程第一名',
+        src: '/images/banner/1.jpg',
+        time: '48课时',
+      },
+      {
+        _id: '01',
+        dec: '学好数理化，走遍天下都不怕',
+        detail: '提高英语成绩备战四级考试',
+        location: '大连理工大学金课建设平台',
+        name: '人文智能',
+        price: '99',
+        rate: '最受欢迎课程第一名',
+        src: '/images/banner/2.jpg',
+        time: '48课时',
+      },
+      {
+        _id: '01',
+        dec: '决胜六级，提高英语成绩',
+        detail: '提高英语成绩备战四级考试',
+        location: '2020年06月16日-2020年07月',
+        name: '学术论文写作',
+        price: '199',
+        rate: '最受欢迎课程第一名',
+        src: '/images/banner/3.jpg',
+        time: '软件周四5-6节',
+      },
+      {
+        _id: '01',
+        dec: '掌握新技术，引领新潮流',
+        detail: '提高英语成绩备战四级考试',
+        location: '2020年06月16日-2020年07月10日',
+        name: '软件项目管理',
+        price: '399',
+        rate: '最受欢迎课程第一名',
+        src: '/images/banner/4.jpg',
+        time: '48课时',
+      },
 
+    ],
   },
 
   /**
